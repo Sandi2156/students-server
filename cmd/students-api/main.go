b5b7e1feaf396d005737721daea22b9da76e5bad
Sandipan Mahata <Sandipan.m@muvin.in>
2025-01-31 19:11:35 +0530

    Added config reader file

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/sandipan/students-api/internal/config"
+)
+
+func main() {
+	// load configuration
+	cfg := config.MustLoad()
+	// database setup
+	// setup routes
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+	// setup server
+	server := http.Server {
+		Addr: cfg.Address,
+		Handler: router,
+	}
+
+	log.Printf("Server listening on %v", cfg.Address)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server.")
+	}
 }
