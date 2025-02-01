0c74e071762fe0b78338019001fcb79029720c57
Sandipan Mahata <Sandipan.m@muvin.in>
2025-02-01 23:48:02 +0530

    done

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -28,4 +28,30 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to start server.")
 	}
+
+	// done := make(chan os.Signal, 1)
+
+	// signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	
+	// func () {
+	// 	log.Printf("Server listening on %v", cfg.Address)
+	// 	err := server.ListenAndServe()
+	// 	if err != nil {
+	// 		log.Fatal("Failed to start server.")
+	// 	}
+	// }()
+
+	// <- done
+
+	// slog.Info("Shutting down the server")
+
+	// ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	// defer cancel()
+
+	// if err := server.Shutdown(ctx); err != nil {
+	// 	slog.Error("We are unable to shutdown the server. ", slog.String("error ", err.Error()))
+	// } 
+
+	// slog.Info("Shut down the server gracefully.")
+
 }
